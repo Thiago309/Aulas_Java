1ce700c9235428b60662b5e99b2750d1379c586e
Thiago_V[309] <116599158+Thiago309@users.noreply.github.com>
2023-11-30 16:46:28 -0300

    30_11

--- AULA_04/AULA_04/src/Array/Exercicio_05.java
+++ AULA_04/AULA_04/src/Array/Exercicio_05.java
@@ -1,10 +1,52 @@
 package Array;
+import java.util.Scanner;
 
 public class Exercicio_05 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		int[] a = new int[10];
+		int[] b = new int[10];
+		int[] c = new int[10];
+		int[] d = new int[10];
+		
+		Scanner valores  = new Scanner(System.in);
+		
+		
+		for (int i = 0; i <= a.length-1; i++) {
+			
+			System.out.printf("\nPreencha o array com os valore %d que deseja: ", i);
+			a[i] = valores.nextInt();
+			
+		}
+		for (int e = 0; e <= a.length-1; e++) {
+			System.out.print(a[e]+ " ");
+		}
+		
+		for (int z = 0; z <= b.length-1; z++) {
+			System.out.printf("\nPreencha o array com os valore %d que deseja: ", z);
+			b[z] = valores.nextInt();
+		}
+		for (int m = 0; m <= b.length-1; m++) {
+			System.out.print(b[m]+ " ");
+		}
+		
+		for (int w = 0; w <= c.length-1; w++) {
+			System.out.printf("\nPreencha o array com os valore %d que deseja: ", w);
+			c[w] = valores.nextInt();
+		}
+		for (int f = 0; f <= c.length-1; f++) {
+			System.out.print(c[f]+ " ");
+		}
+		
+		for (int q = 0; q <= d.length-1; q++) {
+			System.out.printf("\nPreencha o array com os valore %d que deseja: ", q);
+			d[q] = valores.nextInt();
+		}
+		for (int m = 0; m <= d.length-1; m++) {
+			System.out.print(d[m]+ " ");
+		}
+		
 	}
 
 }
--- AULA_04/AULA_04/src/Array/Exercicio_06.java
+++ AULA_04/AULA_04/src/Array/Exercicio_06.java
@@ -1,10 +1,24 @@
 package Array;
+import java.util.Scanner;
 
 public class Exercicio_06 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		float[] a = new float[5];
+		float soma = 0, media = 0;
+		int i = 0;
+		
+		Scanner valores = new Scanner(System.in);
+		
+		for(i = 0; i <= a.length - 1; i++) {
+			
+			System.out.printf("\nDigite o valor %d:",i);
+			a[i] = valores.nextFloat();
+			soma += a[i];
+		}
+		
+		media = soma/i;
+		System.out.printf("Sua media eh: %f",media);	
 	}
-
 }
